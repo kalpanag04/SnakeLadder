3091d70444d5eac17dc7412b27076d66bacefb40
kalpanag04 <kalpanag808@gmail.com>
2022-07-24 10:26:43 +0530

    Initialising player one at position 0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, Welcome to SnakeLadder Game");
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game");
+            SnakeLadder game = new SnakeLadder();
+            game.Board();
+        }
+    }
+}
---
+++ SnakeLadder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class SnakeLadder
+    {
+        public void Start()
+        {
+            //implementing single player start at 0.
+            //variables
+            int position = 0;
+            int playerOne;
+            //initialize player
+            playerOne = position;
+            Console.WriteLine($"player One position is {playerOne}");
+        }
+
+        public void Board()
+        {
+            //at the start
+            Start();
+        }
+
+    }
+}
